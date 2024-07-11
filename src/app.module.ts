89ebcfbc929dc2d5057e27db711f149a868f18c9
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2024-07-11 10:21:48 -0300

    feat: add of user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FirebaseModule } from './shared/providers/firebase/firebase.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [FirebaseModule],
+  imports: [FirebaseModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
