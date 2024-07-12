71709dae920e9a3b3f086809416e2dc485df940a
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2024-07-12 20:48:02 -0300

    fix: fixing identation errors in firebase.api.types

--- src/shared/providers/firebase/types/firebase.api.types.ts
+++ src/shared/providers/firebase/types/firebase.api.types.ts
@@ -1,53 +1,52 @@
-import { WhereFilterOp } from "firebase/firestore";
+import { WhereFilterOp } from 'firebase/firestore';
 
 export interface IQuery<T> {
-    field: string;
-    condition: WhereFilterOp;
-    value: T;
+  field: string;
+  condition: WhereFilterOp;
+  value: T;
 }
 
 type MapFunction = (c: any) => any;
 type FilterFunction = (c: any) => boolean;
 
 export interface IFindAllPayload {
-    collection: string;
-    query?: IQuery<any>[];
-    map?: MapFunction;
-    filter?: FilterFunction;
+  collection: string;
+  query?: IQuery<any>[];
+  map?: MapFunction;
+  filter?: FilterFunction;
 }
 
-export  interface IFindOnePayload {
-    collection: string;
-    id?: string;
-    query?: IQuery<any>[];
-    map?: MapFunction;
+export interface IFindOnePayload {
+  collection: string;
+  id?: string;
+  query?: IQuery<any>[];
+  map?: MapFunction;
 }
 
-export  interface IDelteOnePayload {
-    collection: string;
-    id: string;
+export interface IDelteOnePayload {
+  collection: string;
+  id: string;
 }
 
 export interface IDeleteManyPayload {
-    collection: string;
-    ids?: string[];
-    query?: IQuery<any>[];
+  collection: string;
+  ids?: string[];
+  query?: IQuery<any>[];
 }
 
-
 export interface ICreatePayload {
-    collection: string;
-    payload: any;
+  collection: string;
+  payload: any;
 }
 
 export interface IUpdateOnePayload {
-    collection: string;
-    id: string;
-    payload: any;
+  collection: string;
+  id: string;
+  payload: any;
 }
 
 export interface ISetDocPayload {
-    collection: string;
-    id: string;
-    payload: any;
+  collection: string;
+  id: string;
+  payload: any;
 }
