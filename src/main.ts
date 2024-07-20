9b0febfdacdd77f71d34e2c8841e1434300e2286
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2024-07-19 22:10:12 -0300

    chore: cors has been enabled

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
-
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Docs')
