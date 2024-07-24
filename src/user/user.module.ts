91aa825efeef32102c281cc21147bc4847609391
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2024-07-24 01:02:34 -0300

    feat: register the SpreadSheetService in imports list and set CreateUserService as provider in user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { FirebaseModule } from 'src/shared/providers/firebase/firebase.module';
+import { SpreadsheetService } from 'src/spreadsheet/spreadsheet.service';
+import { CreateUseService } from './create-user.service';
 
 @Module({
-  imports: [FirebaseModule],
+  imports: [FirebaseModule, SpreadsheetService],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, CreateUseService],
   exports: [UserService],
 })
 export class UserModule {}
