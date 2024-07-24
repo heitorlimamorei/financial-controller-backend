98f403808b70414e139314cb2e326c018f61f410
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2024-07-24 01:09:48 -0300

    fix: replacing the SpreadSheetService for the SpreadSheetModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { FirebaseModule } from 'src/shared/providers/firebase/firebase.module';
-import { SpreadsheetService } from 'src/spreadsheet/spreadsheet.service';
 import { CreateUseService } from './create-user.service';
+import { SpreadsheetModule } from 'src/spreadsheet/spreadsheet.module';
 
 @Module({
-  imports: [FirebaseModule, SpreadsheetService],
+  imports: [FirebaseModule, SpreadsheetModule],
   controllers: [UserController],
   providers: [UserService, CreateUseService],
   exports: [UserService],
